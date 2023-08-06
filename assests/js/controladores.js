d5c053b58485f13e5be458fb43218b09a5844912
Luigi <hoyosmejia14o6@gmail.com>
2023-08-06 06:03:46 -0500

    portafolio plantilla

---
+++ assests/js/controladores.js
@@ -0,0 +1,14 @@
+//menu
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("nav-menu_visible");
+    console.log("");
+
+    if (navMenu.classList.contains("nav-menu_visible")){
+        navToggle.setAttribute("aria-label", "Cerrar Menú");
+    } else{
+        navToggle.setAttribute("aria-label", "Abrir menú")
+    }
+})
